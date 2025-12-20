4d44b401fecb71410cfa197cc689a8f24cab4397
snapdrag238 <snapdrag238@gmail.com>
2025-12-20 21:44:29 +0200

    Fix index.js and download.html structure errors

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const audioPlayer = document.getElementById('player');
 const songTitle = document.getElementById('song-title');
 
 const songs = [
-    "song/Colorful cat tinsel garlands.mp3",
+    "song/Colorful-cat-tinsel-garlands.mp3",
     "song/Snowflake.mp3",
-    "song/Carol of the bells.mp3",
-    "song/Deck the halls(instrumental).mp3",
-    "song/We wish you a marry christmas.mp3",
-    "song/Winter time.mp3",
-    "song/Home for the holidays.mp3",
-    "song/Hip hop christmas.mp3"
+    "song/Carol-of-the-bells.mp3",
+    "song/Deck-the-halls(instrumental).mp3",
+    "song/We-wish-you-a-marry-christmas.mp3",
+    "song/Winter-time.mp3",
+    "song/Home-for-the-holidays.mp3",
+    "song/Hip-hop-christmas.mp3"
 ];
 
 let currentSongIndex = 0;
@@ -44,5 +44,4 @@ audioPlayer.onended = nextSong;
 window.addEventListener('load', () => {
     audioPlayer.src = songs[currentSongIndex];
     songTitle.innerText = "Поточний трек: " + getCleanName(songs[currentSongIndex]);
-
 });
